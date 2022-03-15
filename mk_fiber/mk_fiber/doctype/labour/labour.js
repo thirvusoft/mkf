b55b5bc3d38af3d898aae66e916ea0224edd3534
PassionateNaveenkumar <naveen.kumar@thirvusoft.in>
2022-03-15 08:57:07 +0530

    remove labour doctype

--- mk_fiber/mk_fiber/doctype/labour/labour.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, mk and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Labour', {
-	// refresh: function(frm) {
-
-	// }
-});
--- mk_fiber/mk_fiber/doctype/labour/test_labour.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, mk and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestLabour(unittest.TestCase):
-	pass
