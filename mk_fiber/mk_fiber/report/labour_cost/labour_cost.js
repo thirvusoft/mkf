6f6f41871ba4d534ae72fdb02bae93f73e54e6df
PassionateNaveenkumar <naveen.kumar@thirvusoft.in>
2022-03-16 08:42:42 +0530

    Labour costing Report

---
+++ mk_fiber/mk_fiber/report/labour_cost/labour_cost.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, mk and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["labour cost"] = {
+	"filters": [
+
+	]
+};
