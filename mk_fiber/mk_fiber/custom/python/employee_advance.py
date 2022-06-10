057ddb54834eea90fed9241bccf6514c1ff3fac2
root <root@ip-172-26-10-19.ap-south-1.compute.internal>
2022-06-10 06:00:22 +0000

    Site changes

--- mk_fiber/mk_fiber/custom/python/employee_advance.py
+++ mk_fiber/mk_fiber/custom/python/employee_advance.py
@@ -9,13 +9,14 @@ def create_employee_advance(doc,action):
         else:
             frappe.throw("Set Default Employee Advance Account")
     for row in doc.labour_details:
-        new_doc = frappe.new_doc("Employee Advance")
-        new_doc.update({
-            'employee': row.labour_name,
-            'exchange_rate': "1",
-            'purpose':row.working_process,
-            'advance_amount': row.paid_amount,
-            'advance_account': advance_acc
-        })
-        new_doc.insert()
-        new_doc.submit()
+        if(row.paid_amount):
+            new_doc = frappe.new_doc("Employee Advance")
+            new_doc.update({
+                'employee': row.labour_name,
+                'exchange_rate': "1",
+                'purpose':row.working__process,
+                'advance_amount': row.paid_amount,
+                'advance_account': advance_acc
+            })
+            new_doc.insert()
+            new_doc.submit()
--- mk_fiber/mk_fiber/report/labour_costing/labour_costing.py
+++ mk_fiber/mk_fiber/report/labour_costing/labour_costing.py
@@ -5,30 +5,35 @@ def execute(filters=None):
 	from_date = filters.get("from_date")
 	to_date = filters.get("to_date")
 	employee = filters.get("employee_name")	
-		
+	filters={'docstatus':1}
+	ld_filters={'docstatus':1}
+	if(from_date and to_date):
+		filters['posting_date']=['between', [from_date, to_date]]
+	if(employee):
+		ld_filters['labour_name']=employee
 
-	conditions = ""
-	if from_date or to_date or employee:
-		conditions = " where 1=1"
-		if from_date and to_date:
-			conditions += " and se.posting_date between '{0}' and '{1}' ".format(from_date, to_date)
-		
-		if employee:
-			conditions += " and ld.labour_name ='{0}' ".format(employee)
-	report_data = frappe.db.sql("""select ld.labour_name,
-										 ld.working__process,
-										  ld.total_count,
-										  ld.cost_per_piece,
-										  ld.total_cost,
-										  (ld.total_cost - ld.paid_amount) as total_cost 
-								   
-								   from `tabLabour Details` as ld
-								   left outer join `tabStock Entry` as se on
-								   		se.name = ld.parent
-								   {0}
-								   order by posting_date;
-						""".format(conditions))
-	
+
+	ld=frappe.get_all('Labour Details', filters=ld_filters, pluck='name')
+	parent_types=list(set(frappe.get_all('Labour Details', filters=ld_filters, pluck='parenttype')))
+	parent_docs=[]
+	for doctype in parent_types:
+		parent_docs+=frappe.get_all(doctype, filters=filters, pluck='name')
+	report_data=[]
+	for doc in ld:
+		doc=frappe.get_doc('Labour Details', doc)
+		if(doc.parent in parent_docs):
+			report_data.append([
+				doc.labour_name,
+				doc.working__process,
+				doc.total_count,
+				doc.cost_per_piece,
+				doc.total_cost,
+				doc.paid_amount,
+				doc.total_cost - doc.paid_amount
+			])
+
+
+
 	data = [list(i) for i in report_data]
 	emp=[frappe.get_all("Employee",fields=['employee_name'],filters={'name':i[0]})[0]['employee_name'] for i in data]
 	for i in range(len(emp)):data[i][0] = str(emp[i])
@@ -44,6 +49,7 @@ def execute(filters=None):
 		total[3] = round(sum(data[i][3] for i in range(len(data))) / len(data),2)
 		total[4] = sum(data[i][4] for i in range(len(data)))
 		total[5] = sum(data[i][5] for i in range(len(data)))
+		total[6] = sum(data[i][6] for i in range(len(data)))
 		final_data.append(total)
 	columns = get_columns()
 	return columns, final_data
@@ -54,7 +60,8 @@ def get_columns():
 		_("Total Count") + ":int/Labour Details:100",
 		_("Cost Per Piece") + ":Curreny:180",
 		_("Total Cost") + ":Currency:120",
-		_("Balance Amount") + ":Currency:150"
+		_("Paid Amount") + ":Currency:120",
+		_("Balance Amount") + ":Currency:150",
 		]
 	
 	return columns
